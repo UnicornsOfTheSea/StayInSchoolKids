e144e1bdec7eae6b8f817a1ec91ce61122bed041
Olu Fasoro <fasoro.1@napls.us>
2020-05-25 21:44:47 -0400

    Added confirmed urls

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -5,7 +5,7 @@ chrome.runtime.onInstalled.addListener(function() {
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules([{
       conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {hostEquals: 'classroom.google.com'},
+        pageUrl: {hostContains: 'classroom.google.com', '.schoology.com/home'},
       })
       ],
           actions: [new chrome.declarativeContent.ShowPageAction()]
