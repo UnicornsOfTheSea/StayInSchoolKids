b373f4a4fecbaafec0849eeea60fc71cf3ec942e
Olu Fasoro <fasoro.1@napls.us>
2020-05-29 17:08:52 -0400

    added checkboxes(!!!)

--- chrome-extension/mainPopup.js
+++ chrome-extension/mainPopup.js
@@ -12,36 +12,69 @@ btw this is just a framework for the paresr i thought would work, feel free to d
 const renderAssignments = () => {
 	var assignments = [
 		{
-			name: "Essay",
-			due: "05/24/2020"
+			due: "05/24/2020",
+			name: "Essay"
 		},
 		{
-			name: "Math Exercises",
-			due: "05/25/2020"
+			due: "05/25/2020",
+			name: "Math Exercises"
 		},
 		{
-			name: "Lab Report",
-			due: "05/26/2020"
+			due: "05/26/2020",
+			name: "Lab Report"
 		},
 		{
-			name: "History Worksheet",
-			due: "05/27/2020"
+			due: "05/27/2020",
+			name: "History Worksheet"
 		},
 		{
-			name: "Read chapter 1",
-			due: "05/28/2020"
+			due: "05/28/2020",
+			name: "Read chapter 1"
 		}
 	];
 	
 	var table = document.getElementById("dTable");
-	for (var i = 0; i < assignments.length; i++) {
+	for (let i = 0; i < assignments.length + 1; i++) {
 		let newRow = table.insertRow(-1); 
-		for (let property in assignments[i]) {
-			let newCell = newRow.insertCell(0);
-			let text = assignments[i][property];
-			let textNode = document.createTextNode(text);
-			newCell.appendChild(textNode);
+		if(i >= 1) {
+			for (let property in assignments[i-1]) {
+				let newCell = newRow.insertCell(-1);
+				let text = assignments[i-1][property];
+				let textNode = document.createTextNode(text);
+				newCell.appendChild(textNode);
+			}
+		}
+		
+		if (i >= 1 ) {
+			var check = document.createElement('input');
+			var label = document.createElement('label');
+				check.setAttribute('type', 'checkbox');
+				check.setAttribute('value', '0');
+				check.setAttribute('id', 'assignCheck' + i);
+
+				label.setAttribute('for', 'assignCheck' + i);
+				label.appendChild(document.createTextNode(''));
+
+				let newCell = newRow.insertCell(-1);
+				newCell.appendChild(check);
+				newCell.appendChild(label);
 		}
 	}
+	// for (let i=0; i = assignments.length; i++) {
+	// 	let newRow = table.insertRow(-1); 
+	// 	let newCell = newRow.insertCell(0);
+	// 	var check = document.createElement("input");
+	// 	var label = document.createElement("label");
+
+	// 	check.setAttribute('type', 'checkbox');
+	// 	check.setAttribute('value', 'no');
+	// 	check.setAttribute('id', 'assignmentCheck' + i);
+
+	// 	label.setAttribute('for', 'assignmentCheck' + i);
+    // 	label.appendChild(document.createTextNode('Yes'));
+
+	// 	newCell.appendChild(check);
+    // 	newCell.appendChild(label);
+	// }
 } 
 renderAssignments();
