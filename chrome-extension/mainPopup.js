5a6e4788d7a8457883ac46981a49d53bdce168d6
Olu Fasoro <fasoro.1@napls.us>
2020-06-04 14:43:06 -0400

    added 16x16 logo + rearranged assignment info

--- chrome-extension/mainPopup.js
+++ chrome-extension/mainPopup.js
@@ -7,57 +7,55 @@ chrome.storage.sync.get('color', function(data) {
 
 /*function to parse info and store it
 (but for now, we will just use some fake info)
-btw this is just a framework for the paresr i thought would work, feel free to delete if needed
+btw this is just a framework for the parser i thought would work, feel free to delete if needed
 */
 const renderAssignments = () => {
 	var assignments = [
 		{
-			due: "05/24/2020",
-			name: "Essay"
+			name: "Essay",
+			due: "05/24/2020"
 		},
 		{
-			due: "05/25/2020",
-			name: "Math Exercises"
+			name: "Math Exercises",
+			due: "05/25/2020"
 		},
 		{
-			due: "05/26/2020",
-			name: "Lab Report"
+			name: "Lab Report",
+			due: "05/26/2020"
 		},
 		{
-			due: "05/27/2020",
-			name: "History Worksheet"
+			name: "History Worksheet",
+			due: "05/27/2020"
 		},
 		{
-			due: "05/28/2020",
-			name: "Read chapter 1"
+			name: "Read chapter 1",
+			due: "05/28/2020"
 		}
 	];
 	
 	var table = document.getElementById("dTable");
 	for (let i = 0; i < assignments.length + 1; i++) {
 		let newRow = table.insertRow(-1); 
-		if(i >= 1) {
+		if (i >= 1) {
 			for (let property in assignments[i-1]) {
 				let newCell = newRow.insertCell(-1);
 				let text = assignments[i-1][property];
 				let textNode = document.createTextNode(text);
 				newCell.appendChild(textNode);
 			}
-		}
-		
-		if (i >= 1 ) {
 			var check = document.createElement('input');
 			var label = document.createElement('label');
-				check.setAttribute('type', 'checkbox');
-				check.setAttribute('value', '0');
-				check.setAttribute('id', 'assignCheck' + i);
+					
+			check.setAttribute('type', 'checkbox');
+			check.setAttribute('value', '0');
+			check.setAttribute('id', 'assignCheck' + i);
 
-				label.setAttribute('for', 'assignCheck' + i);
-				label.appendChild(document.createTextNode(''));
+			label.setAttribute('for', 'assignCheck' + i);
+			label.appendChild(document.createTextNode(''));
 
-				let newCell = newRow.insertCell(-1);
-				newCell.appendChild(check);
-				newCell.appendChild(label);
+			let newCell = newRow.insertCell(-1);
+			newCell.appendChild(check);
+			newCell.appendChild(label);
 		}
 	}
 } 
